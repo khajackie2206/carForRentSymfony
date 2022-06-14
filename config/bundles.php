3b96b5fb73da41e011bc84f15958b70eaf3f9dbf
khajackie2206 <khajackie2206@gmail.com>
2022-06-14 14:30:23 +0700

    feat: login API feature

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,5 @@ return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
 ];
--- src/Controller/API/AdminController.php
+++ src/Controller/API/AdminController.php
@@ -1,21 +1,18 @@
 <?php
 
-namespace App\Controller;
+namespace App\Controller\API;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
-
 class AdminController extends AbstractController
 {
-    #[Route('/admin', name: 'app_admin')]
+    #[Route('/api/admin', name: 'api_admin')]
     public function index(): Response
     {
-        $user = $this->getUser();
-        return $this->render('admin/index.html.twig', [
-            'grant_name' => 'Admin Role',
-            'user' => $user
+        return $this->json( [
+            'message' => 'This is role_admin',
         ]);
     }
 }
